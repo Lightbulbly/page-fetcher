b457f489c2e0e3c7eb7e979fe85caf0ae7f1f7eb
Noya Onggud <yanuo44@gmail.com>
2021-12-02 14:21:38 -0500

    implemented page downloader

---
+++ fetcher.js
@@ -0,0 +1,39 @@
+// Implement a node app called fetcher.js.
+
+// It should take two command line arguments:
+
+// a URL
+// a local file path
+// It should download the resource at the URL to the local path on your machine. Upon completion, it should print out a message like Downloaded and saved 1235 bytes to ./index.html.
+
+// > node fetcher.js http://www.example.edu/ ./index.html
+// Downloaded and saved 3261 bytes to ./index.html
+const request = require('request');
+const fs = require('fs');
+let content;
+const URL = process.argv[2];
+const filePath = process.argv[3];
+const readline = require('readline');
+
+
+
+content = request(URL, { json: true }, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(URL);
+  return body;
+  // console.log(body.url);
+  // console.log(body.explanation);
+});
+console.log(JSON.stringify(content));
+
+fs.writeFile(filePath, JSON.stringify(content), err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(filePath);
+  console.log("file written successfully");
+});
